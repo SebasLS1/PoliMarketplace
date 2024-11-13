6e55006126c24a63644831b372325950656f3cfd
BaphomeT-T <pdavila0113@gmail.com>
2024-11-12 23:04:47 -0500

    pantalla de inicio finalizada

--- PoliMarketplace/urls.py
+++ PoliMarketplace/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),  # Cambia 'inicio' por 'home'
+    path('', include('home.urls')),  
 ]
