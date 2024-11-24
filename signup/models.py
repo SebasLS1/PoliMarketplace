18f5b92c8084da6e00469662e5335752f838d94a
an-yel <angel.pastaz@epn.edu.ec>
2024-11-23 19:10:06 -0500

    Cracion del campo Correo Electronico y validacion de los demas campos

--- signup/models.py
+++ signup/models.py
@@ -1,4 +1,7 @@
+import re
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.forms import ValidationError
 
 class Sector(models.Model):
     name = models.CharField(max_length=100)
@@ -15,11 +18,18 @@ class Usuario(models.Model):
     birth_date = models.DateField()
     gender = models.CharField(max_length=10)
     sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
-    cedula = models.CharField(max_length=20)
-    password = models.CharField(max_length=100)
+    cedula = models.CharField(max_length=10, unique=True)  # Cédula debe ser única
+    email = models.EmailField(unique=True, null=True)  
+    password = models.CharField(max_length=100, validators=[MinLengthValidator(8)])  # Validar longitud mínima
+
 
     class Meta:
         db_table = 'usuario'
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+    # Validación de la cédula en el modelo
+    def clean(self):
+        if not re.match(r'^\d{10}$', self.cedula):
+            raise ValidationError({'cedula': 'La cédula debe tener 10 dígitos.'})
