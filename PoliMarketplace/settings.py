77a2d5d46cb5117492de6b128816e1064f0953e5
Sebastián Ramos <sebastian.ramos01@epn.edu.ec>
2024-12-01 09:51:15 -0500

    Creación de la ventana de usuario logeado, para poder "vender y comprar"

--- PoliMarketplace/settings.py
+++ PoliMarketplace/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'signup',
     'login',
     'mainPage',
+    'userView',
 ]
 
 MIDDLEWARE = [
@@ -120,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static')
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
