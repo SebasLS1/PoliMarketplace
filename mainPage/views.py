3f932aad4c00538d2522dd409798d47c02827faf
SEBASLS <sebastian.ramos01@epn.edu.ec>
2025-01-30 11:12:15 -0500

    Filtro por condición

--- mainPage/views.py
+++ mainPage/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from publish.models import Categoria, Producto
+from publish.models import Categoria, Producto, Estado
 from .forms import FiltroProductoForm
 
 def lista_productos_view(request):
     # Obtener las categorías
     categorias = Categoria.objects.all()
+    estados= Estado.objects.all()
 
     # Obtener los productos filtrados por zona (sector del usuario) y por categoría
     user = request.user
@@ -20,12 +21,17 @@ def lista_productos_view(request):
     if categoria_id:
         productos_sector = productos_sector.filter(categoria_id=categoria_id)
         otros_productos = otros_productos.filter(categoria_id=categoria_id)
-
+    estado_id= request.GET.get('estado_id', None)
+    if estado_id:
+        productos_sector = productos_sector.filter(estado_id=estado_id)
+        otros_productos = otros_productos.filter(estado_id=estado_id)
+        
     # Ahora pasamos tanto productos sector como los demás productos (sin filtro de zona)
     context = {
         'productos_sector': productos_sector,
         'otros_productos': otros_productos,
         'categorias': categorias,
+        'estados': estados,
     }
 
     return render(request, 'mainPage/mainPage.html', context)
